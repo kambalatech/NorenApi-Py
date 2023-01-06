4fd608b444b4be175167cae7e5c27c6366b666bf
Nikhesh <nikhesh.p@kambala.co.in>
2023-01-06 12:35:27 +0530

    Parameter value changed for get option greek

--- tests/test_GetOption_Greek.py
+++ tests/test_GetOption_Greek.py
@@ -17,6 +17,6 @@ with open('/home/ubuntu/workspace/gitupdation/InterestRate/kambalatech/NorenApi-
 
 ret = api.login(userid = cred['user'], password = cred['pwd'], twoFA=cred['factor2'], vendor_code=cred['vc'], api_secret=cred['apikey'], imei=cred['imei'])
 
-ret = api.option_greek(expiredate ='24-NOV-2022',StrikePrice='150',SpotPrice  = '200',InterestRate  = '100',Volatility = '10',OptionType='5')
+ret = api.option_greek(expiredate ='24-NOV-2022',StrikePrice='150',SpotPrice  = '200',InterestRate  = '100',Volatility = '10',OptionType='CE')
 
 print(ret)
