16c639c2a28c3fb628b99ab958aaada40ef87222
Nikhesh <nikhesh.p@kambala.co.in>
2023-10-12 10:40:03 +0530

    placeorder_timestamp new method added

---
+++ tests/test_placeorder_timestamp.py
@@ -0,0 +1,75 @@
+import time
+import logging
+import datetime as dt
+import sys
+import creds
+from pprint import pprint
+import pyotp
+today = dt.datetime.today().strftime("%b%d")
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from api_helper import NorenApiPy
+
+#logging.basicConfig(level=logging.DEBUG)
+api = NorenApiPy()
+
+creds = {}
+creds['user'] = ''
+creds['pwd'] = ''
+creds['vc'] = ''
+creds['api_secret'] = ''
+creds['imei'] = 'imei-ka'
+
+token = ''
+creds['factor2'] = pyotp.TOTP(token).now()
+
+
+def login():
+    _session = api.login(
+        userid=creds.get('user'),
+        password=creds.get('pwd'),
+        vendor_code=creds.get('vc'),
+        api_secret=creds.get('api_secret'),
+        imei=creds.get('imei'),
+        twoFA=creds.get('factor2'),
+    )
+
+    if _session and _session["stat"] == "Ok":
+        uname = _session["uname"]
+        logging.info(f"Logged in as {uname.split()[0]} ({_session['actid']})\n")
+        return _session
+    else:
+        logging.warning("login failed")
+        logging.debug(_session)
+        sys.exit(1)
+
+def logout():
+    if ret := api.logout()["stat"] == "Ok":
+        logging.info("Logout success")
+    else:
+        logging.info("Failed to log out")
+        logging.debug(ret)
+
+
+login()
+
+tick = time.time()
+ret = api.place_order(
+    buy_or_sell="B",
+    product_type="I",
+    exchange="NSE",
+    tradingsymbol="MIDCAPETF-EQ",
+    quantity=1,
+    discloseqty=0,
+    price_type="MKT",
+    price=0,
+    trigger_price=None,
+    retention="DAY",
+    remarks=f"killswitch_test_latency",
+)
+tock = time.time()
+
+
+pprint(ret)
+print(f"Time to place order  = {tock-tick}s = {(tock-tick)*1000}ms")
+logout()
